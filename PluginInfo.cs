02ac2cae90a45d313d469e8f75461350056f5f29
Lualt <58912038+LualtOfficial@users.noreply.github.com>
2022-08-04 16:05:42 +0200

    i put it in the wrong place :)

--- PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LualtsCameraMod
-{
-    internal class PluginInfo
-    {
-        public const string GUID = "com.lualt.gorillatag.lualtscameramod";
-        public const string Name = "Lualts Camera Mod";
-        public const string Version = "0.0.1";
-    }
-}
