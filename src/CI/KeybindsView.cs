3a7597c4ce156f27573306eaecded467f302123e
@cxzGT on YT <124390124+cxzGT@users.noreply.github.com>
2023-05-10 20:26:50 +0300

    add CI files

---
+++ src/CI/KeybindsView.cs
@@ -0,0 +1,78 @@
+﻿using ComputerInterface;
+using ComputerInterface.ViewLib;
+
+using LualtsCameraMod.Plugin;
+using LualtsCameraMod.ControllerInput;
+
+namespace LualtsCameraMod
+{
+    public class KeybindsView : ComputerView
+    {
+        // This is called when you view is opened
+        public override void OnShow(object[] args)
+        {
+            base.OnShow(args);
+            Text = "Monkey Computer\nMonkey Computer\nMounkey Computer";
+        }
+
+        public void UpdateScreen()
+        {
+            object value = SetText(str =>
+            {
+                str.BeginCenter();
+                str.MakeBar('-', SCREEN_WIDTH, 0, "ffffff10");
+                str.AppendClr("Lualt's Camera Mod", titleColour).EndColor().AppendLine();
+                str.AppendLine("2.0.0");
+                str.MakeBar('-', SCREEN_WIDTH, 0, "ffffff10");
+                str.EndAlign().AppendLines(1);
+                str.MakeBar(' ', SCREEN_WIDTH, 0, "ffffff10");
+
+                str.AppendLine(selectionHandler.GetIndicatedText(0, "Back to Settings"));
+                str.AppendLine();
+
+                
+                str.AppendLines(1);
+                str.MakeBar(' ', SCREEN_WIDTH, 0, "ffffff10");
+            }
+            );
+        }
+
+        public override void OnKeyPressed(EKeyboardKey key)
+        {
+            if (selectionHandler.HandleKeypress(key))
+            {
+                UpdateScreen();
+                return;
+            }
+
+            switch (key)
+            {
+                case EKeyboardKey.Delete:
+                    // "ReturnToMainMenu" will basically switch to the main menu again
+                    ShowView<MainView();
+                    break;
+
+                case EKeyboardKey.Up:
+                    selectionHandler.MoveSelectionUp();
+                    UpdateScreen();
+                    break;
+                case EKeyboardKey.Down:
+                    selectionHandler.MoveSelectionDown();
+                    UpdateScreen();
+                    break;
+            }
+        }
+
+        private void SelectionHandler_OnSelected(int obj)
+        {
+            switch (obj)
+            {
+                case 0:
+                    ShowView<SettingsView>;
+                    break;
+            }
+
+            
+        }
+    }
+}
---
+++ src/CI/MainInstaller.cs
@@ -0,0 +1,22 @@
+﻿using ComputerInterface;
+using ComputerInterface.Interfaces;
+using Zenject;
+
+namespace LualtsCameraMod
+{
+    internal class MainInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            // Bind your mod entry like this
+            Container.Bind<IComputerModEntry>().To<ModEntry>().AsSingle();
+
+            // I just bind another class here to demonstrate adding a command
+            // of course you can request the CommandHandler in any of your types as long as you bind it
+            // notice how I use BindInterfacesAndSelfTo
+            // since MyModCommandManager inherits the IInitializable interface
+            // the class gets instantiated even if no other class needs it
+            Container.BindInterfacesAndSelfTo<MyModCommandManager>().AsSingle();
+        }
+    }
+}
